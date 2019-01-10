ec5c621619ed982747784b5bd575d3fdceecb6a1
Klaudia Butkiewicz <klaudia.butkiewicz09@gmail.com>
2019-01-10 20:53:46 +0100

    RecipeList Component - basic version

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipe: Recipe[] = [
+      new Recipe('Test recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+  ];
 
   constructor() { }
 
