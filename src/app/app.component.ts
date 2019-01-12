b70edddabe6321c20d818772dfc17f12831d9d11
Klaudia Butkiewicz <klaudia.butkiewicz09@gmail.com>
2019-01-12 13:41:14 +0100

    Page nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
+    chosen = 'recipes';
+
+    recipeChosen() {
+        this.chosen = 'recipes';
+    }
+
+    shoppingChosen() {
+        this.chosen = 'shopping';
+    }
 }
+
+
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, Output, EventEmitter} from '@angular/core';
 
-@Component ({
-  selector: 'app-header',
-  templateUrl: './header.component.html'
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'
 })
 
 export class HeaderComponent {
+    @Output() recipeClicked = new EventEmitter();
+    @Output() listClicked = new EventEmitter();
+
+    onRecipeClick() {
+        this.recipeClicked.emit();
+    }
+
+    onShoppingClick() {
+        this.listClicked.emit();
+    }
 
 }
