e9899f15c14f4859e28454db8480192b0d411845
Klaudia Butkiewicz <klaudia.butkiewicz09@gmail.com>
2019-01-12 14:36:25 +0100

    Recipe list & recipe details working

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Recipe } from '../recipe.model';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipe: Recipe[] = [
-      new Recipe('Test recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-      new Recipe('Test recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
-  ];
+    recipe: Recipe[] = [
+        new Recipe('Test recipe', 'This is a simple test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+        new Recipe('Test recipe 2', 'This is a simple test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    ];
 
-  constructor() { }
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    onRecipeSelected(rec: Recipe) {
+        this.recipeWasSelected.emit(rec);
+    }
 
 }
